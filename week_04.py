6b523139b0390b797b3dc0c4973def80c1c42bb8
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-03-15 09:53:09 +1000

    Complete week 4 get age function demo

--- week_04.py
+++ week_04.py
@@ -2,3 +2,19 @@
 Write a function to get a valid age from a user
 Write two lines of main code to show its use
 """
+
+
+def main():
+    age = get_valid_age()
+    print(f"You are {age} years old!")
+
+
+def get_valid_age():
+    age = int(input("Age: "))
+    while age < 0 or age > 100:
+        print("Invalid age")
+        age = int(input("Age: "))
+    return age
+
+
+main()
