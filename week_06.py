586283d0e60aee3f00eb941cd7f0a5d9229f94ae
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-03-29 09:57:54 +1000

    Add week 6 rain chances program

---
+++ week_06.py
@@ -0,0 +1,25 @@
+"""Do this now: Hey Siri, will it rain?
+Given a list of rain chance percentages like:
+[10, 0, 20, 50, 90]
+If any of the chances are 50% or more then the answer is True."""
+
+
+def main():
+    # rain_chances = [10, 0, 20, 50, 90]
+    rain_chances = [10, 0, 20, 0, 50]
+    if is_going_to_rain(rain_chances):
+        print("Yes, get an umbrella")
+    else:
+        print("No rain (Blind Melon)")
+
+
+def is_going_to_rain(rain_chances):
+    """Determine if it will rain or not based on rain chances."""
+    for rain_chance in rain_chances:
+        if rain_chance >= 50:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    main()
